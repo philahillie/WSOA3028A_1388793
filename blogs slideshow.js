1a38f2f4dfe5b18fa907fc42cf838f1780681756
PhilaHillie <philaillustrates@gmail.com>
2021-06-16 23:11:12 +0200

    adding slideshow to blogs page

---
+++ blogs slideshow.js
@@ -0,0 +1,41 @@
+// image slide show as seen on Traversy Media
+
+
+let i=0; 
+let images = [];
+let time = 2500;
+
+// image list
+
+images[0]= '../images/all white.jpg';
+images[1]= '../images/blooming.jpg';
+images[2]= '../images/studio.jpg';
+// images[3]= './images/portrait.jpg';
+
+
+
+//function to change image 
+function changeImg(){
+    document.slide.src = images[i];
+
+    if (i < images.length - 1 ){
+        i++;
+    } else {
+        i=0;
+    }
+
+    setInterval("changeImg()", time);
+
+}
+
+window.addEventListener ('DOMContentLoaded', (_images) => {
+    console.log ('DOM fully loaded and parsed');
+    changeImg();
+});
+
+for (let step = 0; step < 3; step++) {
+    // Runs 5 times, with values of step 0 through 4.
+    console.log(changeImg);
+  }
+
+window.onload = changeImg ;
