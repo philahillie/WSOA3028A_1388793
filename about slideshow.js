aa321e65280a9bdefd11948de18176fb53360f09
PhilaHillie <philaillustrates@gmail.com>
2021-06-17 22:08:46 +0200

    deleting blogs sideshow.js and fixing links

--- about slideshow.js
+++ about slideshow.js
@@ -1,6 +1,7 @@
 
 // image slide show as seen on Traversy Media
 
+//alert('here')
 
 let i=0; 
 let images = [];
@@ -8,10 +9,17 @@ let time = 2500;
 
 // image list
 
-images[0]= './images/all white.jpg';
-images[1]= './images/blooming.jpg';
-images[2]= './images/studio.jpg';
+images[0]= '/images/all white.jpg';
+images[1]= '/images/blooming.jpg';
+images[2]= '/images/studio.jpg';
 // images[3]= './images/portrait.jpg';
+
+//let images = ['./images/all white.jpg','./images/blooming.jpg','./images/studio.jpg']
+//images.forEach(element => {
+//    console.log ('it works', element);
+//});
+
+//for (const image in images)
 
 
 
@@ -30,6 +38,17 @@ function changeImg(){
 }
 
 window.addEventListener ('DOMContentLoaded', (_images) => {
+
+     let pathPrefix = '';
+
+     if (window.location.pathname === '/WSOA3028_1388793/index.html')
+     pathPrefix = './'
+
+     else 
+     pathPrefix = '../';
+
+
+
     console.log ('DOM fully loaded and parsed');
     changeImg();
 });
@@ -39,7 +58,7 @@ for (let step = 0; step < 3; step++) {
     console.log(changeImg);
   }
 
-window.onload = changeImg ;
+window.onclick = changeImg ;
 
 
 
