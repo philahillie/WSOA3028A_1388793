3a51374a7a9dc60e3f5d2b5f8c6275177ae19e4c
PhilaHillie <philaillustrates@gmail.com>
2021-07-03 18:36:07 +0200

    fixng slide show

--- about slideshow.js
+++ about slideshow.js
@@ -3,28 +3,35 @@
 
 //alert('here')
 
-let i=0; 
-let images = [];
-let time = 2500;
+//let i=0; 
+//let images = [];
+//let time = 2500;
 
 // image list
 
-images[0]= '/images/all white.jpg';
-images[1]= '/images/blooming.jpg';
-images[2]= '/images/studio.jpg';
+//images[0]= '/images/all white.jpg';
+//images[1]= '/images/blooming.jpg';
+//images[2]= '/images/studio.jpg';
 // images[3]= './images/portrait.jpg';
 
+const images = [
+    {fileName: "all white.jpg", description: "image of a women in all white"},
+    {fileName: "blooming.jpg", description: "Woman with flowers"},
+    {fileName: "studio.jpg", description: "woman working"}
+
+]
+
 //let images = ['./images/all white.jpg','./images/blooming.jpg','./images/studio.jpg']
-//images.forEach(element => {
-//    console.log ('it works', element);
-//});
+images.forEach(element => {
+   console.log ('it works', element);
+});
 
 //for (const image in images)
 
 
 
 //function to change image 
-function changeImg(){
+function loadImages(){
     document.slide.src = images[i];
 
     if (i < images.length - 1 ){
@@ -33,13 +40,13 @@ function changeImg(){
         i=0;
     }
 
-    setInterval("changeImg()", time);
+    setInterval("loadImages()", time);
 
 }
 
 window.addEventListener ('DOMContentLoaded', (_images) => {
 
-     let pathPrefix = '';
+    let pathPrefix = '';
 
      if (window.location.pathname === '/WSOA3028_1388793/index.html')
      pathPrefix = './'
@@ -50,15 +57,15 @@ window.addEventListener ('DOMContentLoaded', (_images) => {
 
 
     console.log ('DOM fully loaded and parsed');
-    changeImg();
+    loadImages();
 });
 
 for (let step = 0; step < 3; step++) {
-    // Runs 5 times, with values of step 0 through 4.
-    console.log(changeImg);
+     //Runs 5 times, with values of step 0 through 4.
+    console.log(loadImages);
   }
 
-window.onclick = changeImg ;
+window.onload = loadImages ;
 
 
 
@@ -83,3 +90,19 @@ window.onclick = changeImg ;
     
 //};
 
+
+
+//const images = [
+ //   {fileName: "all white.jpg", description: "image of a women in all white"},
+ //   {fileName: "blooming.jpg", description: "Woman with flowers"},
+ //   {fileName: "studio.jpg", description: "woman working"}
+//
+//]
+
+//const loadImages = () => {
+//    let fileToLoad  = "";
+//    if (window.location.pathname === "/WSOA3028_1388793/index.html") {
+//        fileToLoad = "/images/" + image [0].fileName
+//    
+//    }
+//}
