ece5e4ffd771538cd81001c2f70d5678bd5485c2
PhilaHillie <philaillustrates@gmail.com>
2021-07-04 14:39:15 +0200

    deleting incorrect h card

--- menu.js
+++ menu.js
@@ -20,14 +20,6 @@ const menuItems= [
  const renderMenu = ()=> {
 
 
-  //let pathPrefix = " ";
-  
-
-    //if (window.location.pathname === "/WSOA3028A_1388793/index.html")
-     //pathPrefix = "./"
-     //else 
-    //pathPrefix = ("../");
-
     let pathPrefix = window.location.pathname === "/WSOA3028A_1388793/" ? './' : '../'
 
 
@@ -55,19 +47,6 @@ const menuItems= [
 
 
 
-// const renderMenu = ()=> {
- //   const navMenu = document.querySelector(".menuItems");
- //   menuItems.forEach(item => {
-   //       const navBar = document.createElement("nav")
-  //      const newItem = document.createElement("li");
-  //      const newLink = document.createElement("a");
-    //    newLink.href = item.link; 
-      //  newLink.text = item.title;
-        //newItem.appendChild(newLink);
-        //navBar.appendChild (newItem);
-        // navMenu.appendChild (navBar)
-    // });
-// };
 
 
 
